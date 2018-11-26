93843ebf56e64da7971d35934507cbf60f3326f4
Mahmoud <mahmoud.ghorbel@esprit.tn>
2018-11-26 22:17:58 +0100

    adresseModel

---
+++ Epione/Web/Models/AdresseModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models
+{
+    public class AdresseModel
+    {
+        public string street_name { get; set; }
+        public int postal_code { get; set; }
+        public string city { get; set; }
+    }
+}
